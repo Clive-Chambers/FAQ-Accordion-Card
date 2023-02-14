a569f36abfd5c3f425eb907b0b255fde74eafcc6
Clive Chambers <2015chambers@gmail.com>
2023-02-14 14:17:29 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const accordion_btns = document.querySelectorAll('.accordion-question');
+const accordion_contents = document.querySelectorAll('.accordion-body');
+
+accordion_btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+
+        accordion_contents.forEach((acc) => {
+            if (
+                e.target.nextElementSibling !== acc &
+                acc.classList.contains('active')
+            ) {
+                acc.classList.remove('active');
+                accordion_btns.forEach((btn) => btn.classList.remove('active'));
+            }
+        });
+
+        const panel = btn.nextElementSibling;
+        panel.classList.toggle('active');
+        btn.classList.toggle('active');
+    });
+}); 
+
+window.onclick = (e) => {
+    if(!e.target.matches('.accordion-question')) {
+        accordion_btns.forEach((btn) => btn.classList.remove('active'));
+        accordion_contents.forEach((acc) => acc.classList.remove('active'));
+    }
+};
